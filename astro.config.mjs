25ab41c74577975425a1fa542751f1e0a143f380
Marco Finelli <marcofinelli@hotmail.it>
2026-04-19 04:34:03 +0200

    fix: vercel adapter correct mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    mode: 'serverless'
+  })
 });
