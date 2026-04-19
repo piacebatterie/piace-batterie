368904a84f944439b3b031b21e1e43a64a117212
Marco Finelli <marcofinelli@hotmail.it>
2026-04-19 04:32:14 +0200

    fix: force vercel astro server mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
-throw new Error('CONFIG LETTA');
-
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
