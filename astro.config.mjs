eb3b3e3ad54a9dec507f0a2c0e4da3b4472a45cb
Marco Finelli <marcofinelli@hotmail.it>
2026-04-19 04:24:16 +0200

    fix: clean astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
-
-console.log('🚀 CONFIG CARICATA');
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'server',
