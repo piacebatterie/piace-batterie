23e0792f9629a343716e07db855173da542c92c4
Marco Finelli <marcofinelli@hotmail.it>
2026-04-19 04:22:43 +0200

    test config vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
+console.log('🚀 CONFIG CARICATA');
+
 export default defineConfig({
+  output: 'server',
   adapter: vercel(),
-  output: 'server', 
 });
